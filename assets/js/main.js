68b96c6fd7a0854beb4628c39a3de95005f54bb2
Vika Pichugina <v.nedzelskaya@gmail.com>
2019-05-23 09:13:25 +0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,77 @@
+// 15
+
+var string = "Let's start";
+console.log(string);
+
+var number = 365;
+console.log(number);
+
+var bool = true;
+console.log(bool);
+
+var array = [1, 2, 3, 4, 5];
+console.log(array);
+
+var object = {year: '2019', month: 'may', day: '1'};
+console.log(Object.values(object));
+
+var integer = 765;
+var real = integer.toFixed(2);
+console.log(real);
+
+
+var i = 5;
+console.log(++i);  //6
+console.log(i++);  //6
+console.log(i--);  //7
+console.log(--i);  //5
+
+
+var a = 10;
+a += 20;
+console.log(a); //30
+
+var b = 10;
+b -= 5;
+console.log(b); //5
+
+var c = 7;
+c *= 8;
+console.log(c); //56
+
+var d = 10;
+d /= 20;
+console.log(d); //0.5
+
+var e = 467;
+e %= 3;
+console.log(e); //2
+
+
+console.log(Math.PI);
+
+console.log(Math.round(89.279));
+
+console.log(Math.round(Math.random()*10));
+
+console.log(Math.pow(2,5));
+
+var string = 'What is the length of the string';
+console.log(`Length of string is: ${string.length}`);
+
+var string = 'What is the length of the string';
+console.log(string.indexOf('is'));
+
+var string = 'What is the length of the string';
+console.log(string.replace('What is', 'Now we know'));
+
+var string = 'What is the length of the string?';
+console.log(string.toUpperCase());
+
+var string = 'What is the length of the string?';
+console.log(string.toLowerCase());
+
+
+
+//16
+
